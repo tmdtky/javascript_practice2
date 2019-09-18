0af7a0a8de7b47594758ac0831899a70a7d5c9d8
takuya tamada <tmdtky@gmail.com>
2019-09-18 15:39:46 +0900

    追加ボタン押下時、新規タスク追加処理実装

--- main.js
+++ main.js
@@ -1,55 +1,77 @@
 'use strict';
-// ページロード時イベントリスナーを登録
-document.addEventListener('DOMContentLoaded', function () {
+{
   // 追加ボタンクリック時に実行されるイベントリスナーを登録
-  document.getElementById('add').addEventListener('click', function () {
-    // 新規タスクテキストボックス
-    const task = document.getElementById('task');
+  document.getElementById('add-button').addEventListener('click', function () {
+    const newTask = document.getElementById('new-task').value;
 
-    const todo = new Todo(0, 'aaaaa', '作業中');
-    console.log(todo);
-    console.log(todo.id);
-    console.log(todo.comment);
-    console.log(todo.status);
+    // 入力値バリデーション
+    if (newTask === '' || newTask === 'タスクを入力して下さい') {
+      document.getElementById('new-task').value = "タスクを入力して下さい";
+    }
 
+    // 新しいタスクの追加
+    addTableRow(newTask);
   });
-});
 
-// ID, コメント, 状態に関するクラスを作成、
-// インスタンス化した値を配列に格納し、リスト出力;
-// 必要なもの
-// クラス ID、コメント、状態
-// enum 状態：作業中、完了、削除
-class Todo {
+  // 新しいタスクを追加する関数
+  function addTableRow(newTask) {
+    const tbl = document.getElementById('tbl');
+    const row = tbl.insertRow(Todo.counter);
+    const cellId = row.insertCell(0);
+    const cellComment = row.insertCell(1);
+    const cellStatus = row.insertCell(2);
 
-  constructor(id, comment, status) {
-    this._id = id;
-    this._comment = comment;
-    this._status = status;
+    const status = '作業中';
+    const todo = new Todo(Todo.counter, newTask, status)
+    cellId.textContent = Todo.counter;
+    cellComment.textContent = todo.comment;
+    cellStatus.textContent = todo.status;
+    Todo.counter++;
+
+    document.getElementById('new-task').value = '';
   }
 
-  get id() {
-    return this._id;
+  // ToDoクラス
+  class Todo {
+
+    static counter = 0;
+
+    constructor(id, comment, status) {
+      this._id = id;
+      this._comment = comment;
+      this._status = status;
+    }
+
+    get id() {
+      return this._id;
+    }
+
+    set id(id) {
+      this._id = id;
+    }
+
+    get comment() {
+      return this._comment;
+    }
+
+    set comment(comment) {
+      this._comment = comment;
+    }
+
+    get status() {
+      return this._status;
+    }
+
+    set status(status) {
+      this._status = status;
+    }
   }
 
-  set id(id) {
-    this._id = id;
+  // 状態（完了、作業中、削除）の列挙型
+  let Status = {
+    complete: 0,
+    inWork: 1,
+    delete: 2
   }
 
-  get comment() {
-    return this._comment;
-  }
-
-  set comment(comment) {
-    this._comment = comment;
-  }
-
-  get status() {
-    return this._status;
-  }
-
-  set status(status) {
-    this._status = status;
-  }
-};
-
+}
